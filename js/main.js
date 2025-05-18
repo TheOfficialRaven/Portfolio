8679cfc53c3da1b456cdb92f9f1b758c8019fb70
Mark Gór <sath_@outlook.hu>
2025-05-18 18:59:07 +0200

    hero szekció majdnem kész, szolgaltatasok valtoztatasai

--- js/main.js
+++ js/main.js
@@ -118,15 +118,16 @@ function initVantaBackground() {
       mouseControls: true,
       touchControls: true,
       gyroControls: false,
-      minHeight: 200.00,
-      minWidth: 200.00,
+      minHeight: 100.00,
+      minWidth: 100.00,
       scale: 1.00,
       scaleMobile: 1.00,
       color: 0x00c9a7,
       backgroundColor: 0x0d0d0d,
       points: 14.00,
       maxDistance: 20.00,
-      spacing: 18.00
+      spacing: 18.00,
+      showDots: false
     });
     console.log("✅ VANTA elindult");
   } else {
